e93e01a346113144bad07bf6da109a9e23098e05
seirra <hack@192.168.0.103>
2017-04-02 21:04:07 +0800

    修改

--- loanCalculate/loanCalculate/ViewController.swift
+++ loanCalculate/loanCalculate/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loanAmountField: UITextField!
+    @IBOutlet weak var loanTermField: UITextField!
+    @IBOutlet weak var interestRateField: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBAction func simpleButton(_ sender: Any) {
+        //实例化
+        let calculate1 = simpleCalculate()
+        //！表示强制解析，有可能为nil
+        result.text = calculate1.calculate(loanAmount: Double(loanAmountField.text!)!, years:Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).description
+    }
+    //MARK:视图加载完成
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //MARK:接到内存警告
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +34,10 @@ class ViewController: UIViewController {
 
 }
 
+class simpleCalculate{
+    func calculate(loanAmount:Double, years:Int, interestRate:Double) -> Double{
+        let rate = interestRate / 100
+        let interest = loanAmount * rate * Double(years)
+        return loanAmount + interest
+    }
+}
