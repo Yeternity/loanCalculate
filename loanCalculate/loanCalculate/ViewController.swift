66e76077ae8f6442e78164c8faa2faeabad290c3
seirra <hack@192.168.0.100>
2017-04-03 20:28:59 +0800

    异常优化

--- loanCalculate/loanCalculate/ViewController.swift
+++ loanCalculate/loanCalculate/ViewController.swift
@@ -18,7 +18,27 @@ class ViewController: UIViewController {
         //实例化
         let calculate1 = simpleCalculate()
         //！表示强制解析，有可能为nil
-        result.text = calculate1.calculate(loanAmount: Double(loanAmountField.text!)!, years:Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).description
+        guard let loanTermField = loanTermField.text, let loanAmountField = loanAmountField.text, let interestRateField = interestRateField.text
+            else{
+                return
+        }
+        guard let loanTerm = Int(loanTermField), let loanAmount = Double(loanAmountField), let interestRate = Double(interestRateField) else {
+            print("format error")
+            return
+        }
+        result.text = calculate1.calculate(loanAmount: loanAmount, years: loanTerm, interestRate: interestRate).money
+        //result.text = calculate1.calculate(loanAmount: Double(loanAmountField.text!)!, years:Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).money
+    }
+    @IBAction func compoundButton(_ sender: Any) {
+        let calculate2 = compoundCalculate()
+        result.text = calculate2.calculate(loanAmount: Double(loanAmountField.text!)!, years: Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).description
+    }
+    
+    // shift + command + 0 调出官方参考文档
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        loanAmountField.resignFirstResponder()
+        loanTermField.resignFirstResponder()
+        interestRateField.resignFirstResponder()
     }
     //MARK:视图加载完成
     override func viewDidLoad() {
@@ -41,3 +61,31 @@ class simpleCalculate{
         return loanAmount + interest
     }
 }
+
+class compoundCalculate{
+    func calculate(loanAmount: Double, years: Int, interestRate: Double) -> Double{
+        let rate = interestRate / 100
+        let interest = pow(1.0+rate, Double(years))
+        return loanAmount * interest
+    }
+}
+
+//extension 扩展，可以扩展功能，扩展对象包括class, enum, struct
+extension Double{
+    var money: String{
+        // 3.0之前很多带NS前缀的函数，在3.0中都去掉了
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        //定义一个可选类型 ？ ，表明它可以为nil
+        let result: String?
+        result = formatter.string(from: NSNumber(value:self))
+        if result == nil{
+            return "format error"
+        }
+        return result!
+    }
+}
+
+//command + shift + K 调出或隐藏键盘
+//option(alt) + click 查看元素介绍
+//command(win) + click 查看源码
